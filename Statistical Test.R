1af3d80c504d2cee01a53aa63dd0839491e5e8c8
mlregan <mlkrouth@hotmail.com>
2024-03-21 19:03:46 -0400

    Fixed formatting

--- Statistical Test.R
+++ Statistical Test.R
@@ -1,1 +1,5 @@
-res <- prop.test(x = c(12632, 10437), n = c(26320537, 21002553), p = NULL, alternative = "two.sided", correct = TRUE)print(res)res$p.valueres$estimateres$conf.int
+res <- prop.test(x = c(12632, 10437), n = c(26320537, 21002553), p = NULL, alternative = "two.sided", correct = TRUE)
+print(res)
+res$p.value
+res$estimate
+res$conf.int
